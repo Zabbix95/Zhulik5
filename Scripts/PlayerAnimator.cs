cde36d6965bb8b0711f7d45e920c5ee237390ee8
Zabbix95 <76256786+Zabbix95@users.noreply.github.com>
2021-05-26 19:14:45 +0500

    Add files via upload

---
+++ Scripts/PlayerAnimator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(PlayerMover))]
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField] private PlayerMover _mover;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();        
+    }
+    private void OnEnable() 
+    {
+        _mover.SpeedChanged += OnSpeedChanged;
+    }
+
+    private void OnDisable() 
+    {
+        _mover.SpeedChanged -= OnSpeedChanged;
+    }
+
+    private void OnSpeedChanged(float speed)
+    {
+        _animator.SetFloat("Speed", speed);
+    }
+}
